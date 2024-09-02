3a1548962ebd1b6252c4d6009ce7d308abe9df87
Scope0204 <scope0204@gmail.com>
2024-09-02 19:47:54 +0900

    feat: SpringBoot 프로젝트 생성

---
+++ scope/spring-project/hanghae-board/src/main/java/hanghae/board/HanghaeBoardApplication.java
@@ -0,0 +1,13 @@
+package hanghae.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanghaeBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanghaeBoardApplication.class, args);
+	}
+
+}
