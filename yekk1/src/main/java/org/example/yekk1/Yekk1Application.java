56a2183ce7153533dd17f7082c56841eba80a303
yekk1 <daeyoi96@gmail.com>
2024-09-03 17:11:02 +0900

    chore: Spring Boot Project 생성

---
+++ yekk1/src/main/java/org/example/yekk1/Yekk1Application.java
@@ -0,0 +1,14 @@
+package org.example.yekk1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Yekk1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Yekk1Application.class, args);
+		System.out.println("Hello Spring!!!");
+	}
+
+}
