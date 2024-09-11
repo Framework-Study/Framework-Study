979fedefe984c88da4ddcf737f69f19c764f4381
yekk1 <daeyoi96@gmail.com>
2024-09-11 11:39:50 +0900

    feat: hanghaememo 실습 모듈 생성

---
+++ yekk1/hanghaememo/src/main/java/com/sparta/hanghaememo/HanghaememoApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.hanghaememo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanghaememoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanghaememoApplication.class, args);
+	}
+
+}
